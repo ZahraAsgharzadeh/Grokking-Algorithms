7f3893b208a127b5fcc641a79d7a6bcfaa2f1a66
Zahra Asgharzadeh <zahraasgharzadeh1377@gmail.com>
2023-09-02 13:51:59 +0330

    MODIFY -> finish chapter 1

--- python/Chapter_1/Practice_3.py
+++ python/Chapter_1/Practice_3.py
@@ -6,6 +6,13 @@ Question 1.3 ->
     ****************
     نام یک نفر را در نظر دارید و می خواهید شماره تلفن آن شخص را در دفترچه تلفن پیدا کنید .
 
+    جستجوی دودویی
+
 """
 
 # Answer 1.3
+
+import math
+
+def search(input) :
+    return math.log(input, 2) # O (log n)
